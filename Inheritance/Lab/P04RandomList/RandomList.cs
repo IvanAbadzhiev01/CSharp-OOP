629e9bf082d7caae820daaa0da7fd8832739b34c
IvanAbadjiev <ivan087611@gmail.com>
2023-05-28 21:14:30 +0300

    • Inheritance; • Class Hierarchies; • Accessing Base Class Members; • Reusing Classes; • Type of Class Reuse.

---
+++ Inheritance/Lab/P04RandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random random = new Random();
+            string randomString = this[random.Next(0, Count)];
+            this.Remove(randomString);
+
+
+            return randomString;
+        }
+
+
+    }
+}
---
+++ Inheritance/Lab/P04RandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Add("4");
+            list.Add("5");
+
+            string randomString = list.RandomString();
+        }
+    }
+}
