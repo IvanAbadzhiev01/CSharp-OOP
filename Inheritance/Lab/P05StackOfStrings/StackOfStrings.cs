2eb3f2cb60c199701c684849a2a8568fc78528b1
IvanAbadjiev <ivan087611@gmail.com>
2023-05-28 21:14:52 +0300

    • Inheritance; • Class Hierarchies; • Accessing Base Class Members; • Reusing Classes; • Type of Class Reuse.

---
+++ Inheritance/Lab/P05StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => this.Count == 0;
+
+
+        public Stack<string> AddRange(IEnumerable<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                this.Push(item);
+            }
+
+            return this;
+        }
+    }
+}
