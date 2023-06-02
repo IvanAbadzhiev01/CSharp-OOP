e2aa8cc514469d02ad10431c84c06682ab64a80e
IvanAbadjiev <ivan087611@gmail.com>
2023-06-02 20:51:38 +0300

    • Inheritance; • Class Hierarchies; • Accessing Base Class Members; • Reusing Classes; • Type of Class Reuse.

---
+++ Inheritance/Exercise/Person/Person.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System;
+
+namespace Person
+{
+    public class Person
+    {
+
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; private set; }
+        public virtual int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public override string ToString() => $"Name: {this.Name}, Age: {this.Age}";
+
+
+    }
+}
