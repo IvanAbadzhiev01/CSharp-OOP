66a82a700b932ca376fb0032ec510e88513f924b
IvanAbadjiev <ivan087611@gmail.com>
2023-10-02 08:18:25 +0300

    • What are Exceptions?; • Handling Exceptions; • Raising (Throwing) Exceptions; • Best Practices; • Creating Custom Exceptions.

---
+++ Exceptions and Error Handling/Lab/P02EnterNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿List<int> numbers = new();
+int start = 1;
+int end = 100;
+while (numbers.Count < 10)
+{
+    try
+    {
+        start = ReadNumber(start, end);
+
+    }
+    catch (ArgumentException ex)
+    {
+
+        Console.WriteLine(ex.Message);
+    }
+    catch (FormatException fx)
+    {
+        Console.WriteLine(fx.Message);
+    }
+}
+Console.WriteLine(string.Join(", ", numbers));
+
+
+
+int ReadNumber(int start, int end)
+{
+    string input = Console.ReadLine();
+
+    if (int.TryParse(input, out int number))
+    {
+
+        if (number > start && number < end)
+        {
+            numbers.Add(number);
+
+        }
+        else
+        {
+            throw new ArgumentException($"Your number is not in range {start} - 100!");
+        }
+
+    }
+    else
+    {
+        throw new FormatException("Invalid Number!");
+    }
+
+
+    return number;
+}
