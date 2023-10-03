ddae491cd784cb396caea0a3cb7ceab150312ea7
IvanAbadjiev <ivan087611@gmail.com>
2023-10-03 21:44:53 +0300

    • What are Exceptions?; • Handling Exceptions; • Raising (Throwing) Exceptions; • Best Practices; • Creating Custom Exceptions.

---
+++ Exceptions and Error Handling/Lab/P04SumOfIntegers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Linq;
+
+string[] elementsInput = Console.ReadLine()
+    .Split();
+int sumOfValidInteger = 0;
+
+foreach (var element in elementsInput)
+{
+    try
+    {
+        if (int.TryParse(element, out int number))
+        {
+            sumOfValidInteger += number;
+            Console.WriteLine($"Element '{element}' processed - current sum: {sumOfValidInteger}");
+        }
+        else
+        {
+
+            int intpars = int.Parse(element);
+
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine($"The element '{element}' is in wrong format!");
+        Console.WriteLine($"Element '{element}' processed - current sum: {sumOfValidInteger}");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine($"The element '{element}' is out of range!");
+        Console.WriteLine($"Element '{element}' processed - current sum: {sumOfValidInteger}");
+    }
+
+
+}
+
+
+
+Console.WriteLine($"The total sum of all integers is: {sumOfValidInteger}");
+
