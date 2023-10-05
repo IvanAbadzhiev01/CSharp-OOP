a7e9d22399ab05d4d8efd142fc1218ac221b37d3
IvanAbadjiev <ivan087611@gmail.com>
2023-10-05 10:45:49 +0300

    SOLID
    • Single Responsibility;
    • Open/Closed;
    • Liskov Substitution;
    • Interface Segregation;
    • Dependency Inversion.

---
+++ SOLID/Lab/P01.Stream_Progress/Photo.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.AccessControl;
+
+namespace P01.Stream_Progress
+{
+    public class Photo : IFile
+    {
+        private string author;
+        public Photo(string author, int length, int bytesSent)
+        {
+            this.author = author;
+            Length = length;
+            BytesSent = bytesSent;
+        }
+
+        public int Length { get; private set; }
+
+        public int BytesSent { get; private set; }
+    }
+}
---
+++ SOLID/Lab/P01.Stream_Progress/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+            List<IFile> files = new();
+            files.Add(new File("Ivan", 180, 10));
+            files.Add(new Music("Ivan", "Blest", 190, 10));
+            files.Add(new Photo("Ivan", 100, 10));
+
+
+
+            foreach (var file in files)
+            {
+                StreamProgressInfo progressInfo = new(file);
+
+                System.Console.WriteLine(progressInfo.CalculateCurrentPercent());
+            }
+
+        }
+    }
+}
