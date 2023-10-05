1f026ba9150e03327bfb360bc3c7a1354141d5d1
IvanAbadjiev <ivan087611@gmail.com>
2023-10-05 10:46:55 +0300

    SOLID
    • Single Responsibility;
    • Open/Closed;
    • Liskov Substitution;
    • Interface Segregation;
    • Dependency Inversion.

---
+++ SOLID/Lab/P02.Graphic_Editor/GraphicEditor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P02.Graphic_Editor
+{
+    public class GraphicEditor
+    {
+
+        //private IShape shape;
+        //public GraphicEditor(IShape shape)
+        //{
+        //    this.shape = shape;
+
+        //}
+
+
+
+        public void DrawShape(IShape shape)
+        {
+            Console.WriteLine($"I'm {shape.GetType().Name}");
+            //if (shape is Circle)
+            //{
+            //    Console.WriteLine("I'm Circle");
+            //}
+            //else if (shape is Rectangle)
+            //{
+            //    Console.WriteLine("I'm Recangle");
+            //}
+            //else if (shape is Square)
+            //{
+            //    Console.WriteLine("I'm Square");
+            //}
+        }
+    }
+}
---
+++ SOLID/Lab/P02.Graphic_Editor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace P02.Graphic_Editor
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<IShape> shapes = new();
+
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
+            shapes.Add(new Square());
+
+            GraphicEditor graphic = new();
+
+            foreach (var shape in shapes)
+            {
+                graphic.DrawShape(shape);
+            }
+
+        }
+    }
+}
