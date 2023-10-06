b9586439dcd029dbdf528683c208ce8526ac0474
IvanAbadjiev <ivan087611@gmail.com>
2023-10-06 18:15:16 +0300

    SOLID
    • Single Responsibility;
    • Open/Closed;
    • Liskov Substitution;
    • Interface Segregation;
    • Dependency Inversion.

---
+++ SOLID/Lab/P04.Recharge/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace P04.Recharge
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee workerEmployee = new Employee("Tishko");
+            Worker workerRobot = new Robot("K2", 101);
+
+            workerEmployee.
+        }
+    }
+}
