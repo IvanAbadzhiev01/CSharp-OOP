528e31b437555101bce897b5a84e4e849ff7c36e
IvanAbadjiev <ivan087611@gmail.com>
2023-10-09 22:32:58 +0300

    Reflection and Attributes
    • Reflection API;
    • Attributes.

---
+++ Reflection and Attributes/Lab/P01Stealer/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fildsName)
+        {
+
+
+            Type type = Type.GetType(className);
+            object classInstance = Activator.CreateInstance(type);
+
+            FieldInfo[] fields = type.GetFields((BindingFlags)60);
+            StringBuilder sb = new();
+            sb.AppendLine($"Class under investigation: {type.FullName}");
+
+
+            foreach (FieldInfo field in fields)
+            {
+                if (fildsName.Any(f => f == field.Name))
+                {
+                    sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+                }
+            }
+            return sb.ToString().TrimEnd();
+
+        }
+    }
+}
