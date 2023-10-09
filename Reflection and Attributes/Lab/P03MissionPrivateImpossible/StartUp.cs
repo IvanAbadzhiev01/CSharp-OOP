cf917ea49e2fa9804a179d1dd1e39ce9012141bf
IvanAbadjiev <ivan087611@gmail.com>
2023-10-09 22:33:45 +0300

    Reflection and Attributes
    • Reflection API;
    • Attributes.

---
+++ Reflection and Attributes/Lab/P03MissionPrivateImpossible/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Stealer;
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        Spy spy = new Spy();
+        //string result = spy.StealFieldInfo("Stealer.Hacker", new string[] { "username", "password" });
+        //string result = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+        string result = spy.RevealPrivateMethods("Stealer.Hacker");
+        Console.WriteLine(result);
+    }
+}
