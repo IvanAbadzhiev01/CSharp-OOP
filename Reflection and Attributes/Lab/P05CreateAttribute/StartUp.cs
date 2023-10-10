69480aecd450b476f179fed14e473cdbb4afd737
IvanAbadjiev <ivan087611@gmail.com>
2023-10-10 10:58:22 +0300

    Reflection and Attributes
    • Reflection API;
    • Attributes.

---
+++ Reflection and Attributes/Lab/P05CreateAttribute/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Ivan")]
+    public class StartUp
+    {
+        [Author("GoShow")]
+        static void Main(string[] args)
+        {
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
